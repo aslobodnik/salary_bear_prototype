103244a1f2be2232bb9c18325f2cc5ef2cda8cf0
slobo <aslobodnik@gmail.com>
2015-07-03 13:31:12 -0500

    add search functionality and url creation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request, url_for
 import pandas as pd
+from slugify import slugify
 
 app = Flask(__name__)
 
@@ -12,6 +13,10 @@ df.columns = ['organization'
                 ,'title'
                 ,'salary'
                 ,'state']
+
+df['full_name'] = df['first_name'] + ' ' + df['middle_name'] + ' '\
+                    + df['last_name']
+
 df['id'] = df.index
 
 @app.route("/")
@@ -22,13 +27,26 @@ def home():
 @app.route('/<state>/<organization>/<name>-<int:person_id>')
 def profile(state,organization,name,person_id):
     person = df.iloc[person_id]
-    return render_template('profile.html',person=person)
+    url = generate_url(person_id)
+    return render_template('profile.html',person=person, url=url)
 
 @app.route('/search/', methods=['POST'])
 def search():
     search_text = request.form['search_box']
-    search_results = df.iloc[int(search_text)]
-    return render_template('search_results.html',person=search_results)
+    search_results = search_by_name(search_text)
+    return render_template('search_results.html', person=search_results)
+
+def search_by_name(search_string):
+    results = df[df.full_name.str.contains(search_string.upper())]
+    return results
+
+def generate_url(person_id):
+    person = df.iloc[person_id]
+    url = '/' + slugify(person.state) + '/'
+    url += slugify(person.organization) + '/'
+    url += slugify(person.full_name) + '-'
+    url += str(person_id)
+    return url
 
 if __name__ == "__main__":
     app.run(debug=True)
