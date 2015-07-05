a681d03c92dca20589b1ee9be6708ad799e7c203
slobo <aslobodnik@gmail.com>
2015-07-04 19:55:07 -0500

    add search functionality

--- app.py
+++ app.py
@@ -19,6 +19,27 @@ df['full_name'] = df['first_name'] + ' ' + df['middle_name'] + ' '\
 
 df['id'] = df.index
 
+
+def create_person(df_row):
+    person = {}
+    person['organization'] = df_row.organization
+    person['department'] = df_row.department
+    person['last_name'] = df_row.last_name
+    person['first_name'] = df_row.first_name
+    person['middle_name'] = df_row.middle_name
+    person['title'] = df_row.title
+    person['salary'] = df_row.salary
+    person['state'] = df_row.state
+    person['full_name'] = df_row['first_name'] + ' ' + df_row['middle_name'] +\
+            ' ' + df_row['last_name']
+    return person
+
+def create_people(df):
+    people = []
+    for index, row in df.iterrows():
+        people.append(create_person(row))
+    return people
+
 @app.route("/")
 def home():
     person = df.iloc[0]
@@ -34,11 +55,16 @@ def profile(state,organization,name,person_id):
 def search():
     search_text = request.form['search_box']
     search_results = search_by_name(search_text)
-    return render_template('search_results.html', person=search_results)
+    people = create_people(search_results)
+    return render_template('search_results.html', people=people,
+            search_text=search_text)
 
 def search_by_name(search_string):
-    results = df[df.full_name.str.contains(search_string.upper())]
-    return results
+    search_string = search_string.upper().split()
+    results = df.last_name != None
+    for word in search_string:
+        results &= df.full_name.str.contains(word)
+    return df[results]
 
 def generate_url(person_id):
     person = df.iloc[person_id]
